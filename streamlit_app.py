3ecab8ef766910421183bdd7a763a67d7244acc5
Sabar <ton_email@exemple.com>
2026-05-08 19:33:11 +0200

    Connexion finale Legion100

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from brain import * # On importe ton intelligence Sabar Digital
 
-st.set_page_config(page_title='Sabar Empire', page_icon='í¿ï¸')
-st.title('í¿ï¸ Sabar Empire Design - V3.0 DEEP_SEA')
+st.set_page_config(page_title='Sabar Empire', page_icon='Ã­Â¿ÂÃ¯Â¸Â')
+st.title('Ã­Â¿ÂÃ¯Â¸Â Sabar Empire Design - V3.0 DEEP_SEA')
 
 url = st.text_input('Entrez l URL cible pour l audit Legion100')
 
@@ -10,7 +10,7 @@ if st.button('LANCER L ANALYSE'):
     if url:
         with st.spinner('Analyse DEEP_SEA en cours...'):
             # Ici on appelle ta logique de brain.py
-            st.success(f'Audit terminÃ© pour {url}. Score de conversion : 98%')
+            st.success(f'Audit terminÃÂ© pour {url}. Score de conversion : 98%')
             st.balloons()
     else:
         st.error('Sabar, l Empire exige une URL.')
